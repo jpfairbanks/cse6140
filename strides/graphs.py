e79b28fadf6856cce49fbe894049d3483a584a09
James Fairbanks <james.fairbanks@gatech.edu>
2013-09-09 18:39:12 -0400

    Add perelement figures write figures into subdirectory

--- strides/graphs.py
+++ strides/graphs.py
@@ -3,9 +3,27 @@ import matplotlib
 matplotlib.use("pdf")
 import matplotlib.pyplot as plt
 import sys
+import os.path
 
-df = pd.read_csv(sys.stdin, " ", header=None, index_col=0)
+figdir = "figures"
+
+df = pd.read_csv(sys.stdin, " ", header=None, index_col=0,
+                 names=[2**(i) for i in range(6)]+["rand"])
 print(df)
-print(df["X2"]/2**df.index)
+#print(df["X2"]/2**df.index)
 df.plot(logy=True)
-plt.savefig("graph.pdf")
+plt.title("run time for array access")
+plt.xlabel("scale")
+plt.ylabel("seconds")
+plt.savefig(os.path.join([figdir,"graph.pdf"]))
+
+plt.figure()
+sizes = 2**df.index
+print(sizes)
+petf = (df.T/sizes).T
+print( petf )
+petf.plot(logy=True)
+plt.title("normalized running time")
+plt.xlabel("scale")
+plt.ylabel("nanoseconds per element")
+plt.savefig(os.path.join([figdir,"perelement.pdf"]))
