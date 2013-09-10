8099739a58819a9dab3159556e59f953ad5cef59
James Fairbanks <james.fairbanks@gatech.edu>
2013-09-10 12:02:47 -0400

    fix paths and take a file as command line argument

--- strides/graphs.py
+++ strides/graphs.py
@@ -5,17 +5,24 @@ import matplotlib.pyplot as plt
 import sys
 import os.path
 
+if len(sys.argv) > 1 :
+    fp = open(sys.argv[1], "r")
+else:
+    fp = sys.stdin
+
 figdir = "figures"
 
-df = pd.read_csv(sys.stdin, " ", header=None, index_col=0,
-                 names=[2**(i) for i in range(6)]+["rand"])
+df = pd.read_csv(fp, " ", header=None, index_col=0,
+                 names=[2**(i) for i in range(6)]+["rand", "list"])
 print(df)
 #print(df["X2"]/2**df.index)
 df.plot(logy=True)
 plt.title("run time for array access")
 plt.xlabel("scale")
 plt.ylabel("seconds")
-plt.savefig(os.path.join([figdir,"graph.pdf"]))
+figpath = os.path.join(figdir,"graph.pdf")
+print(figpath)
+plt.savefig(figpath)
 
 plt.figure()
 sizes = 2**df.index
@@ -26,4 +33,4 @@ petf.plot(logy=True)
 plt.title("normalized running time")
 plt.xlabel("scale")
 plt.ylabel("nanoseconds per element")
-plt.savefig(os.path.join([figdir,"perelement.pdf"]))
+plt.savefig(os.path.join(figdir,"perelement.pdf"))
