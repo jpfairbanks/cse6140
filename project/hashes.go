f42aecab257504f8e2c3f43b7cc532de3aec1d70
James <jpfairbanks@gmail.com>
2013-09-19 22:54:39 -0400

    thinking about using crypto/rand to get primes

--- project/hashes.go
+++ project/hashes.go
@@ -2,7 +2,7 @@
 Author: James Fairbanks
 Liscence: BSD
 
- we need to find primes and compute a*x+b mod p as the hash,
+we need to find primes and compute a*x+b mod p as the hash,
 then we need to test that these hashes are indeed pairwise independent for
 distinct values of a and b.
 */
@@ -11,6 +11,34 @@ package hashes
 
 import(
 	"fmt"
+	"crypto/rand"
 )
 
+//Hash: a struct for hashing things
+type struct Hash {
+	var a, b, p int
+}
 
+/*
+func (h Hash) Write() int, error {
+	buff := make([]int, buffsize)
+	fmt.Println("write is fake implemented")
+	return 1, error
+}
+*/
+
+//Return: the hash for a single input
+func (h Hash) hash(x int) int {
+	return (a * x + b) % p
+}
+
+//New: create a new hash and make sure that it is valid
+func New(a,b,p int) Hash {
+	
+}
+
+//New: create a new hash and make sure that it is valid
+func New(a,b int) func(int) int {
+	p := rand.Prime(crypto.Reader, 64)
+	return func(x int)int {return (a * x + b) % p}
+}
