432c5e011c4af0d79b3118dcbbd999861a43145f
James <jpfairbanks@gmail.com>
2013-09-20 19:21:50 -0400

    documenting: CMSketch methods

--- project/main.go
+++ project/main.go
@@ -6,7 +6,8 @@ import (
 	"math/rand"
 )
 
-//CMSketch: A streaming data structure for tracking the elements of a long vector in a space efficient manner.
+//CMSketch: A streaming data structure for tracking the elements of 
+//a long vector in a space efficient manner.
 //Provides update and query operations
 type CMSketch struct {
 	Width   int64
@@ -15,6 +16,9 @@ type CMSketch struct {
 	Counter Matrix
 }
 
+//NewCMSketch: Allocates a new sketch
+//Does not create the hashes. We leave that up to you so that 
+//the consumer can pick hashes according to different schema.
 func NewCMSketch(Width, Depth int64) CMSketch {
 	Hash := make([]hashes.Hash, Depth)
 	Counter := NewMatrix(Width, Depth)
@@ -22,14 +26,18 @@ func NewCMSketch(Width, Depth int64) CMSketch {
 	return cms
 }
 
+//RandomHashes: Make uniformly random hashes from a user provided
+//stream of random numbers.
 func RandomHashes(r *rand.Rand, Depth int64) []hashes.Hash {
-
 	hslice := make([]hashes.Hash, Depth)
 	for i, _ := range hslice {
 		hslice[i] = hashes.Rand(r)
 	}
 	return hslice
 }
+
+//UpdateSerial: Insert a single item into the sketch with a count.
+//If counts can be negative, then you mist estimate differently
 func (cms *CMSketch) UpdateSerial(position int64, count int64) {
 	for i, h := range cms.Hash {
 		cms.Counter.Add(int64(i), h.Apply(position)%cms.Width, count)
