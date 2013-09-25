ae8eef3363a310c8545660454a1450cf0419c80a
James <jpfairbanks@gmail.com>
2013-09-24 22:10:34 -0400

    bring for loop into shell script

--- project/run.sh
+++ project/run.sh
@@ -1,4 +1,7 @@
 PROG='/Total Loss:/ {split($2, a, "/"); print a[1]/a[2], a[1], a[2]}'
 #'BEGIN {print "Avg.Loss", "TotalLoss", "Count"}
 SEP=":"
-awk -F$SEP "$PROG" -
+for (( i = 1; i < 100; i+=10 )); do
+    ./project -depth $i -width 700 -efactor 8 |\
+        awk -F$SEP "$PROG" -
+done
